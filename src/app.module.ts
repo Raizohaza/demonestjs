a9a21b2e430fe985f2496ab6c788e9670a54bc1c
Pham Phong Phu Cuong <phamcuong2751@gmail.com>
2022-10-10 19:39:20 +0700

    change version mysql

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,20 +6,22 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { DataSource } from 'typeorm';
 import { FilmModule } from './film/film.module';
 import { ConfigModule } from '@nestjs/config';
+import { truncate } from 'fs';
 @Module({
   imports: [
     ConfigModule,
     ActorModule,
     FilmModule,
     TypeOrmModule.forRoot({
-      type: 'mysql',
+      type: "mysql",
       host: "103.74.117.138",
-      port: 21424,
-      username: "21hcb-hcmus",
+      port: 3306,
+      username: "cuong-21hcb",
       password: "21424003",
       database: "demonestjs",
       entities: [],
       synchronize: false,
+      insecureAuth: true,
       autoLoadEntities: true,
     }),
   ],
