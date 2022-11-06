a6cf82cd72a6bd59f840beb209e3c8eaeb626dd3
Phạm Phong Phú Cường <phamcuong2751@gmail.com>
2022-11-06 22:48:00 +0700

    add api register

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,17 +1,18 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from 'src/entities/User';
-
 import { Repository } from 'typeorm';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import * as bcrypt from 'bcrypt';
+
 export type UserTest = any;
 @Injectable()
 export class UserService {
   constructor(
     @InjectRepository(User)
     private UserRepository: Repository<User>,
-  ) {}
+  ) { }
   private readonly users = [
     {
       userId: 1,
@@ -25,6 +26,18 @@ export class UserService {
     },
   ];
   async create(userData: CreateUserDto) {
+    userData.password = await bcrypt.hash(userData.password, 10);
+
+    //check existing user
+    const existingUser = await this.UserRepository
+      .createQueryBuilder('user')
+      .where('user.email = :email', { email: userData.email })
+      .getOne();
+
+    if (existingUser) {
+      throw new HttpException('User already exists', HttpStatus.BAD_REQUEST);
+    }
+
     const newUser = await this.UserRepository.create(userData);
     await this.UserRepository.save(newUser);
     return newUser;
@@ -40,6 +53,7 @@ export class UserService {
   async findOneByName(username: string): Promise<UserTest | undefined> {
     return this.users.find((user) => user.username === username);
   }
+
   update(id: number, updateUserDto: UpdateUserDto) {
     return `This action updates a #${id} user`;
   }
